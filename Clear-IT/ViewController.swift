89ef484c905977ac5d1dce8f7c43fa56604a405f
RayZ <zhang.rayz@gmail.com>
2019-03-28 15:56:39 -0700

    delete derive data with button

--- Clear-IT/ViewController.swift
+++ Clear-IT/ViewController.swift
@@ -22,6 +22,47 @@ class ViewController: NSViewController {
     }
   }
 
+  @IBAction func didTapDelete(_ sender: Any) {
+    deleteDerivedData()
+  }
+  
+  func deleteDerivedData() {
+    print("Delete that shit")
+    let task = Process()
 
+    let path = getPathToScript()
+    
+    task.launchPath = "/bin/sh"
+    task.arguments = [path, "-d"]
+    task.launch()
+    task.waitUntilExit()
+    print("Done")
+  }
+  
+  /*
+   Script Options
+   OPTIONS:
+   -h           Show this help message
+   -a           Remove all Xcode archives
+   -d           Remove everything in DerivedData folder
+   -D           Remove everything in DeviceSupport folder
+   -s           Remove simulator data
+   -A           Remove all of the above(archived, DerivedData and simulator data)
+   --dry-run    Dry run mode prints which directories would be cleared but don't remove any files
+ */
+  /// Gets the path to the script the xcode-clean script
+  private func getPathToScript() -> String {
+    let THIS_FILES_PATH:String = #file
+    let subPaths = THIS_FILES_PATH.split(separator: "/")
+    let pathsWithoutCurrentFile = Array(subPaths[0..<(subPaths.count - 1)])
+    let path = "/" + pathsWithoutCurrentFile.joined(separator: "/")
+    return "\(path)/scripts/xcode-clean.sh"
+  }
+  
+  func cleanProduct() {
+    print("Clean that shit")
+    let task = Process()
+    task.launchPath = "/usr/bin/xcodebuild"
+    task.arguments = ["-workspace", "\"Yelp.xcworkspace\""]
+  }
 }
-
