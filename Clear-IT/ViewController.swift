8bb4d2391f02081f8fcaaf0d33bc33e8ee8bed49
rohan <rohg@yelp.com>
2019-03-28 10:46:20 -0700

    Initial Commit

---
+++ Clear-IT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Clear-IT
+//
+//  Created by Rohan Gupta on 3/28/19.
+//  Copyright © 2019 Rohan Gupta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
---
+++ Clear-ITTests/Clear_ITTests.swift
@@ -0,0 +1,34 @@
+//
+//  Clear_ITTests.swift
+//  Clear-ITTests
+//
+//  Created by Rohan Gupta on 3/28/19.
+//  Copyright © 2019 Rohan Gupta. All rights reserved.
+//
+
+import XCTest
+@testable import Clear_IT
+
+class Clear_ITTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
